d4365f7aaf876e872fc781d6e63e9359665dd96b
Sharla Gelfand <sharla.gelfand@gmail.com>
2020-02-19 12:19:07 -0500

    Add function to generate and open carbon

--- functions.R
+++ functions.R
@@ -25,3 +25,39 @@ update_repo_readme <- function(date = Sys.Date()) {
 
   write(all_to_add, file = "README.md", append = TRUE)
 }
+
+generate_carbon_images <- function(date = Sys.Date()) {
+  `%>%` <- magrittr::`%>%`
+
+  date_readme_md <- readLines(paste0(date, "/README.md"))
+
+  functions_text <- date_readme_md %>%
+    tibble::as_tibble() %>%
+    dplyr::mutate(newline_after_chunk_end = value == "" & dplyr::lag(value) == "```") %>%
+    dplyr::filter(!(value %in% c("``` r", "```")) & !newline_after_chunk_end) %>%
+    dplyr::mutate(function_type = dplyr::case_when(
+      stringr::str_detect(value, "i know this one") ~ "old",
+      stringr::str_detect(value, "new to me") ~ "new"
+    )) %>%
+    tidyr::fill(function_type, .direction = "down") %>%
+    dplyr::filter(!is.na(function_type)) %>%
+    dplyr::filter(!stringr::str_detect(value, "i know this one|new to me")) %>%
+    dplyr::mutate(value = stringr::str_replace(value, "    ##", "#>")) %>%
+    dplyr::group_by(function_type) %>%
+    dplyr::mutate(leading_or_trailing_newline = value == "" & dplyr::row_number() %in% c(1, dplyr::n())) %>%
+    dplyr::filter(!leading_or_trailing_newline) %>%
+    dplyr::ungroup()
+
+  carbon_links <- functions_text %>%
+    dplyr::group_split(function_type) %>%
+    purrr::map(~ dplyr::pull(.x, value)) %>%
+    purrr::map(~ carbonate::carbon$new(.x)) %>%
+    purrr::map_chr(~ .x$uri())
+
+  for(i in seq_along(carbon_links)) {
+  utils::browseURL(
+    url = carbon_links[[i]],
+    browser = getOption("browser")
+  )
+  }
+}
